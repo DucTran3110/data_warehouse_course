0c229a7e82692003835d2b4993018b91fc30e8da
duc_tran <duc.tranminh3110@gmail.com>
2022-11-12 18:48:43 +0700

    split cte

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,22 @@
+WITH dim_product__source as (
+  SELECT 
+    *
+    FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  ),
+  dim_product__rename_col as (
+  SELECT
+    stock_item_id as product_key,
+    stock_item_name as product_name,
+    brand as brand_name
+    FROM dim_product__source
+  ),
+  dim_product__cast AS (
+  SELECT
+    CAST(product_key AS INTEGER) AS product_key,
+    CAST(product_name AS STRING) AS product_name,
+    CAST(brand_name AS STRING) AS brand_name
+  FROM dim_product__rename_col
+  )
 SELECT 
-  CAST(stock_item_id AS INTEGER) as product_key,
-  CAST(stock_item_name AS STRING) as product_name,
-  CAST(brand AS STRING) as brand_name 
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  *
+FROM dim_product__cast
