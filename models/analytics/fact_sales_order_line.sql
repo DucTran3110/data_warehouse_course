68bcd6931fee0a4483b558b532548da745f49415
duc_tran <duc.tranminh3110@gmail.com>
2022-11-12 17:27:54 +0700

    add product key

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -2,7 +2,8 @@ SELECT
   order_line_id as sales_order_line_key,
   quantity,
   unit_price,
-  quantity*unit_price as gross_amount
+  quantity*unit_price as gross_amount,
+  stock_item_id as product_key
 FROM
   `vit-lam-data.wide_world_importers.sales__order_lines`
   
