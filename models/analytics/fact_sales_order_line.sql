e4697e3b687531623490e20345b4eda62b7d2939
duc_tran <duc.tranminh3110@gmail.com>
2022-11-12 18:15:31 +0700

    cast fact sales order

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,9 +1,9 @@
 SELECT
-  order_line_id as sales_order_line_key,
-  quantity,
-  unit_price,
-  quantity*unit_price as gross_amount,
-  stock_item_id as product_key
+  CAST(order_line_id AS INTEGER) as sales_order_line_key,
+  CAST(quantity AS INTEGER) as quantity,
+  CAST(unit_price AS NUMERIC) as unit_price,
+  CAST(quantity*unit_price AS NUMERIC) as gross_amount,
+  CAST(stock_item_id AS INTEGER) as product_key
 FROM
   `vit-lam-data.wide_world_importers.sales__order_lines`
   
