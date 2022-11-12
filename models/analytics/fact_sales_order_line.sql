7b93d1d489288895b2807f94bb956591feffcae7
duc_tran <duc.tranminh3110@gmail.com>
2022-11-12 18:28:01 +0700

    split calculation

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -2,7 +2,7 @@ SELECT
   CAST(order_line_id AS INTEGER) as sales_order_line_key,
   CAST(quantity AS INTEGER) as quantity,
   CAST(unit_price AS NUMERIC) as unit_price,
-  CAST(quantity*unit_price AS NUMERIC) as gross_amount,
+  CAST(quantity AS INTEGER)*CAST(unit_price AS NUMERIC) as gross_amount,
   CAST(stock_item_id AS INTEGER) as product_key
 FROM
   `vit-lam-data.wide_world_importers.sales__order_lines`
