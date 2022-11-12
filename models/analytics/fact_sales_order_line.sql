3ac162e200aeaf5c5cf1f39411f57f029bb13051
duc_tran <duc.tranminh3110@gmail.com>
2022-11-12 16:17:11 +0700

    select order line id

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,3 +1,7 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+SELECT
+  order_line_id as sales_order_line_key,
+  quantity,
+  unit_price
+FROM
+  `vit-lam-data.wide_world_importers.sales__order_lines`
+  
