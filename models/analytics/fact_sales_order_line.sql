92ff0841a121aee025560ba02e8f326980f0bc91
duc_tran <duc.tranminh3110@gmail.com>
2022-11-14 11:33:47 +0700

    lesson-0105b

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,9 +1,30 @@
+WITH 
+  fact_sales_order_lines__source as (
+  SELECT
+    *
+  FROM 
+    `vit-lam-data.wide_world_importers.sales__order_lines`
+  ),
+  fact_sales_order_lines__rename_col as (
+  SELECT
+    order_line_id as sales_order_line_key,
+    quantity,
+    unit_price,
+    stock_item_id as product_key
+  FROM
+    fact_sales_order_lines__source
+  ),
+  fact_sales_order_lines__cast as (
+  SELECT 
+    CAST(sales_order_line_key AS INTEGER) as sales_order_line_key,
+    CAST(quantity AS INTEGER) as quantity,
+    CAST(unit_price AS NUMERIC) as unit_price,
+    CAST(product_key AS INTEGER) as product_key
+  FROM
+    fact_sales_order_lines__rename_col
+)
 SELECT
-  CAST(order_line_id AS INTEGER) as sales_order_line_key,
-  CAST(quantity AS INTEGER) as quantity,
-  CAST(unit_price AS NUMERIC) as unit_price,
-  CAST(quantity AS INTEGER)*CAST(unit_price AS NUMERIC) as gross_amount,
-  CAST(stock_item_id AS INTEGER) as product_key
+  *,
+  quantity*unit_price as gross_amount,
 FROM
-  `vit-lam-data.wide_world_importers.sales__order_lines`
-  
+  fact_sales_order_lines__cast
