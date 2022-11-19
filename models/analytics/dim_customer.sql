980db0dc24ddeaf56e7a6cb2072687433e98ca82
duc_tran <duc.tranminh3110@gmail.com>
2022-11-19 16:15:42 +0700

    lesson-0106a

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,27 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+WITH
+  dim_customer__source AS (
+    SELECT
+      *
+    FROM
+      `vit-lam-data.wide_world_importers.sales__customers`
+    ),
+  dim_customer__rename_col AS (
+    SELECT
+      customer_id as customer_key,
+      customer_name
+    FROM 
+      dim_customer__source
+    ),
+  dim_customer__cast AS (
+    SELECT
+      CAST(customer_key AS INTEGER) as customer_key,
+      CAST(customer_name AS STRING) as customer_name
+    FROM
+      dim_customer__rename_col
+    )
+
+SELECT
+  customer_key,
+  customer_name
+FROM
+  dim_customer__cast
