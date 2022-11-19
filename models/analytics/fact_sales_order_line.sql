257c120f1fe04b1d26e3229d37aa7e7566a9f28f
duc_tran <duc.tranminh3110@gmail.com>
2022-11-19 16:01:40 +0700

    lesson-0105b-2

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,30 +1,41 @@
 WITH 
   fact_sales_order_lines__source as (
-  SELECT
-    *
-  FROM 
-    `vit-lam-data.wide_world_importers.sales__order_lines`
-  ),
+    SELECT
+      *
+    FROM 
+      `vit-lam-data.wide_world_importers.sales__order_lines`
+    ),
   fact_sales_order_lines__rename_col as (
-  SELECT
-    order_line_id as sales_order_line_key,
-    quantity,
-    unit_price,
-    stock_item_id as product_key
-  FROM
-    fact_sales_order_lines__source
-  ),
+    SELECT
+      order_line_id as sales_order_line_key,
+      quantity,
+      unit_price,
+      stock_item_id as product_key
+    FROM
+      fact_sales_order_lines__source
+    ),
   fact_sales_order_lines__cast as (
-  SELECT 
-    CAST(sales_order_line_key AS INTEGER) as sales_order_line_key,
-    CAST(quantity AS INTEGER) as quantity,
-    CAST(unit_price AS NUMERIC) as unit_price,
-    CAST(product_key AS INTEGER) as product_key
-  FROM
-    fact_sales_order_lines__rename_col
-)
-SELECT
-  *,
-  quantity*unit_price as gross_amount,
+    SELECT 
+      CAST(sales_order_line_key AS INTEGER) as sales_order_line_key,
+      CAST(quantity AS INTEGER) as quantity,
+      CAST(unit_price AS NUMERIC) as unit_price,
+      CAST(product_key AS INTEGER) as product_key
+    FROM
+      fact_sales_order_lines__rename_col
+    ),
+  fact_sales_order_line__calculate_fact as (
+    SELECT
+      *,
+      quantity*unit_price as gross_amount
+    FROM
+      fact_sales_order_lines__cast
+    )
+
+SELECT 
+  sales_order_line_key,
+  quantity,
+  unit_price,
+  product_key,
+  gross_amount,
 FROM
-  fact_sales_order_lines__cast
+  fact_sales_order_line__calculate_fact
