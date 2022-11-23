0a6e3a531312fd1c47d0a8a6183b464c8a5f6e42
duc_tran <duc.tranminh3110@gmail.com>
2022-11-23 19:26:28 +0700

    add-supplier-key-dim_product

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -7,16 +7,21 @@ WITH dim_product__source as (
   SELECT
     stock_item_id as product_key,
     stock_item_name as product_name,
-    brand as brand_name
+    brand as brand_name,
+    supplier_id as supplier_key
     FROM dim_product__source
   ),
   dim_product__cast AS (
   SELECT
     CAST(product_key AS INTEGER) AS product_key,
     CAST(product_name AS STRING) AS product_name,
-    CAST(brand_name AS STRING) AS brand_name
+    CAST(brand_name AS STRING) AS brand_name,
+    CAST(supplier_key AS INTEGER) AS supplier_key
   FROM dim_product__rename_col
   )
 SELECT 
-  *
+  product_key,
+  product_name,
+  brand_name,
+  supplier_key
 FROM dim_product__cast
