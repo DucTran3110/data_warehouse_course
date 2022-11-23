1cfd82eabd0bb7ad75c488e9d39367506ae6c9a4
duc_tran <duc.tranminh3110@gmail.com>
2022-11-23 19:19:08 +0700

    lesson-0107a

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,27 @@
-SELECT 
+WITH dim_supplier_source as 
+(
+  SELECT 
   *
-FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+  FROM 
+  `vit-lam-data.wide_world_importers.purchasing__suppliers`
+),
+  dim_supplier_rename AS 
+(
+  SELECT
+  supplier_id AS supplier_key,
+  supplier_name
+  FROM
+  dim_supplier_source
+),
+  dim_supplier_cast AS
+(
+  SELECT
+  CAST(supplier_key AS INTEGER) as supplier_key,
+  CAST(supplier_name AS STRING) as supplier_name
+  FROM
+  dim_supplier_rename
+)
+SELECT 
+  supplier_key,
+  supplier_name
+FROM dim_supplier_cast
