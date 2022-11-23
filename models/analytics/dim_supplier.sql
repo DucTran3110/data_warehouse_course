d375b741434f6849f9209758de370f0e2a7b3268
duc_tran <duc.tranminh3110@gmail.com>
2022-11-23 19:30:14 +0700

    adjust-__

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,27 +1,27 @@
-WITH dim_supplier_source as 
+WITH dim_supplier__source as 
 (
   SELECT 
   *
   FROM 
   `vit-lam-data.wide_world_importers.purchasing__suppliers`
 ),
-  dim_supplier_rename AS 
+  dim_supplier__rename AS 
 (
   SELECT
   supplier_id AS supplier_key,
   supplier_name
   FROM
-  dim_supplier_source
+  dim_supplier__source
 ),
-  dim_supplier_cast AS
+  dim_supplier__cast AS
 (
   SELECT
   CAST(supplier_key AS INTEGER) as supplier_key,
   CAST(supplier_name AS STRING) as supplier_name
   FROM
-  dim_supplier_rename
+  dim_supplier__rename
 )
 SELECT 
   supplier_key,
   supplier_name
-FROM dim_supplier_cast
+FROM dim_supplier__cast
