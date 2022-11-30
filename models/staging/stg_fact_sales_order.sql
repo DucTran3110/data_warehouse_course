299f08b7b90c9c4fc7f0a14fdc96aac54a5dc42d
duc_tran <duc.tranminh3110@gmail.com>
2022-11-30 19:13:30 +0700

    lesson-0110a-stg_fact_sales_order

--- models/staging/stg_fact_sales_order.sql
+++ models/staging/stg_fact_sales_order.sql
@@ -7,22 +7,25 @@ WITH
     ),
   fact_sales_order__rename_col as (
     SELECT
-      order_id as sales_order_key,
-      customer_id as customer_key
+      order_id as sales_order_key
+      , customer_id as customer_key
+      , picked_by_person_id as picked_by_person_key
     FROM
       fact_sales_order__source
     ),
   fact_sales_order__cast as (
     SELECT
-      CAST(sales_order_key as INTEGER) as sales_order_key,
-      CAST(customer_key as INTEGER) as customer_key
+      CAST(sales_order_key as INTEGER) as sales_order_key
+      ,CAST(customer_key as INTEGER) as customer_key
+      ,CAST(picked_by_person_key as INTEGER) as picked_by_person_key
     FROM
       fact_sales_order__rename_col
     )
 
 SELECT
-  sales_order_key,
-  customer_key
+  sales_order_key
+  , customer_key
+  , COALESCE(picked_by_person_key,0) as picked_by_person_key
 FROM
   fact_sales_order__cast
   
