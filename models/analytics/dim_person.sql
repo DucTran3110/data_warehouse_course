ead628235650164b7b99fc12586f0db618bf64de
duc_tran <duc.tranminh3110@gmail.com>
2022-11-30 19:05:08 +0700

    lesson-0110a-dim_person

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,3 +1,28 @@
-SELECT 
+WITH dim_person__source as 
+(
+  SELECT 
   *
-FROM `vit-lam-data.wide_world_importers.application__people`
+  FROM 
+  `vit-lam-data.wide_world_importers.application__people`
+),
+  dim_person__rename AS 
+(
+  SELECT
+    person_id AS person_key
+    , full_name
+  FROM
+  dim_person__source
+),
+  dim_person__cast AS
+(
+  SELECT
+    CAST(person_key AS INTEGER) as person_key
+    , CAST(full_name AS STRING) as full_name
+  FROM
+  dim_person__rename
+)
+SELECT 
+  person_key
+  , full_name
+FROM dim_person__cast
+
