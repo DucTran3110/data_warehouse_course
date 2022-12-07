0846e93533cf7e9e4b1cc942ceae6c96ac0c4ac2
duc_tran <duc.tranminh3110@gmail.com>
2022-12-07 20:14:54 +0700

    add-fact-order_line

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -7,38 +7,53 @@ WITH
     ),
   fact_sales_order_lines__rename_column as (
     SELECT
-      order_line_id as sales_order_line_key,
-      quantity,
-      unit_price,
-      order_id as sales_order_key,
-      stock_item_id as product_key
+      order_line_id as sales_order_line_key
+      ,description
+      ,order_id as sales_order_key
+      ,stock_item_id as product_key
+      ,package_type_id as package_type_key
+      ,quantity
+      ,unit_price
+      ,tax_rate
+      ,picked_quantity
+      ,picking_completed_when
     FROM
       fact_sales_order_lines__source
     ),
   fact_sales_order_lines__cast_type as (
     SELECT 
-      CAST(sales_order_line_key AS INTEGER) as sales_order_line_key,
-      CAST(quantity AS INTEGER) as quantity,
-      CAST(unit_price AS NUMERIC) as unit_price,
-      CAST(sales_order_key AS INTEGER) as sales_order_key,
-      CAST(product_key AS INTEGER) as product_key
+      CAST(sales_order_line_key AS INTEGER) as sales_order_line_key
+      ,CAST(description as STRING) as description
+      ,CAST(sales_order_key AS INTEGER) as sales_order_key
+      ,CAST(product_key AS INTEGER) as product_key
+      ,CAST(package_type_key as INTEGER) as package_type_key
+      ,CAST(quantity AS INTEGER) as quantity
+      ,CAST(unit_price AS NUMERIC) as unit_price
+      ,CAST(tax_rate AS NUMERIC) as tax_rate
+      ,CAST(picked_quantity AS INTEGER) as picked_quantity
+      ,CAST(picking_completed_when AS DATE) as picking_completed_when
     FROM
       fact_sales_order_lines__rename_column
     ),
   fact_sales_order_line__calculate_measure as (
     SELECT
-      *,
-      quantity*unit_price as gross_amount
+      *
+      ,quantity*unit_price as gross_amount
     FROM
       fact_sales_order_lines__cast_type
     )
 
 SELECT 
   fact_line.sales_order_line_key
-  ,fact_line.quantity
-  ,fact_line.unit_price
+  ,fact_line.description
   ,fact_line.sales_order_key
   ,fact_line.product_key
+  ,fact_line.package_type_key
+  ,fact_line.quantity
+  ,fact_line.unit_price
+  ,fact_line.tax_rate
+  ,fact_line.picked_quantity
+  ,fact_line.picking_completed_when
   ,fact_line.gross_amount
   ,COALESCE(fact_header.customer_key,-1) as customer_key
   ,COALESCE(fact_header.picked_by_person_key,-1) as picked_by_person_key
