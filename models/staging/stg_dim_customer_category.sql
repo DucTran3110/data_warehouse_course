c87c89d95b89e46df9f5f964a5b61bfdb69d80cc
duc_tran <duc.tranminh3110@gmail.com>
2022-12-07 21:18:13 +0700

    add_undefine_record-stg_dim_customer_category

--- models/staging/stg_dim_customer_category.sql
+++ models/staging/stg_dim_customer_category.sql
@@ -3,23 +3,34 @@ WITH
     SELECT *
     FROM `vit-lam-data.wide_world_importers.sales__customer_categories`
   ),
-  dim_customer_category__rename AS(
+  dim_customer_category__rename_column AS(
     SELECT 
       customer_category_id as customer_category_key
       , customer_category_name
     FROM
       dim_customer_category__source
   ),
-  dim_customer_category__cast AS(
+  dim_customer_category__cast_type AS(
     SELECT
       CAST(customer_category_key as INTEGER) as customer_category_key
       , CAST(customer_category_name as STRING) as customer_category_name
     FROM
-      dim_customer_category__rename
+      dim_customer_category__rename_column
+  ),
+  dim_customer_category__add_undefine_record AS(
+    SELECT
+      customer_category_key
+      ,customer_category_name
+    FROM
+      dim_customer_category__cast_type
+    Union all 
+    SELECT
+      0 as customer_category_key
+      ,'Undefined' as customer_category_name
   )
 
 SELECT
   customer_category_key
   , customer_category_name
 FROM
-  dim_customer_category__cast
+  dim_customer_category__add_undefine_record
