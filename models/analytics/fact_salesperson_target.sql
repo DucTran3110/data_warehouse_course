87fd33767c2aceec4da138328da5e559fea5d0e4
duc_tran <duc.tranminh3110@gmail.com>
2022-12-22 10:14:17 +0700

    fact_salesperson_target_Step4

--- models/analytics/fact_salesperson_target.sql
+++ models/analytics/fact_salesperson_target.sql
@@ -17,13 +17,23 @@ WITH
       ,target_revenue
       ,gross_amount
     FROM
-      {{ref('stg_fact_target_salesperson')}} as fact_target
+      {{ref('stg_fact_salesperson_target')}} as fact_target
     FULL OUTER JOIN 
       fact_salesperson_target__gross_amount as fact_so
     USING (year_month,salesperson_person_key)
+  ),
+  fact_salesperson_target__calculation as (
+    SELECT
+      year_month
+      ,salesperson_person_key
+      ,target_revenue
+      ,gross_amount
+      ,(gross_amount/Ifnull(target_revenue,0)) as achievement
+    FROM
+      fact_salesperson_target__join
   )
 
 SELECT 
   *
 FROM
-  fact_salesperson_target__join
+  fact_salesperson_target__calculation
